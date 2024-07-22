80392bb9c4bf5240ee1444f392a1532ceb2c1e96
junreythedatadigger <junrey.o.ansing@gmail.com>
2024-07-22 20:17:48 +0800

    Basic calculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,17 @@
-equation = input('Enter equation: ')
-print(equation)
+# Basic calculator that performs addition, subtraction, multiplication and division of two numbers
+def calculate(first_num, operator, second_num):
+    if operator == '*':
+        return first_num * second_num
+    elif operator == '/':
+        return first_num / second_num
+    elif operator == '+':
+        return first_num + second_num
+    elif operator == '-':
+        return first_num - second_num
+
+first_num = int(input('Enter the first number: '))
+operator = input('Enter the operator (+-*/): ')
+second_num = int(input('Enter the second number: '))
+
+result = calculate(first_num, operator, second_num)
+print(f'Result: {first_num} {operator} {second_num} = {result}')
