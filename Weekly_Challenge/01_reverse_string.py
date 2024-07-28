10d133040e34e0147b734ff59eefd35671734048
junreythedatadigger <junrey.o.ansing@gmail.com>
2024-07-28 22:00:43 +0800

    Fully functional simple quiz

--- Weekly_Challenge/01_reverse_string.py
+++ Weekly_Challenge/01_reverse_string.py
@@ -6,6 +6,7 @@ def reverse_string(original_string):
         reversed_string += original_string[-(i+1)]
     return reversed_string
 
+
 # original_string = "Hello world, hello Philippines!!"
 original_string = input("Enter the string: ")
 print(f'Re-versed string: {reverse_string(original_string)}')
